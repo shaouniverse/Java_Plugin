9aa86d07b2caa645b9f73b7b9e5714dd67d70e85
ShaoShuai <shaopart@163.com>
2019-04-18 14:46:34 +0800

    使用Spring Aware （重新实现接口获取Spring所提供的资源）

---
+++ src/main/java/com/ShaoShuai/example/demo/a03/a03_01/a03_01_02/AwareConfig.java
@@ -0,0 +1,10 @@
+package com.ShaoShuai.example.demo.a03.a03_01.a03_01_02;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ShaoShuai.example.demo.a03.a03_01.a03_01_02")
+public class AwareConfig {
+
+}
---
+++ src/main/java/com/ShaoShuai/example/demo/a03/a03_01/a03_01_02/Main.java
@@ -0,0 +1,14 @@
+package com.ShaoShuai.example.demo.a03.a03_01.a03_01_02;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(AwareConfig.class);
+		AwareService awareService = context.getBean(AwareService.class);
+		awareService.outputResult();
+		
+		context.close();
+	}
+}
