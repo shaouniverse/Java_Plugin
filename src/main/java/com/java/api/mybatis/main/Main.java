a41addca5a41270c095505156346dc7b7b132d73
ShaoShuai <shaopart@163.com>
2019-04-23 14:57:52 +0800

    sqlSession需要提交才能更改数据否则会进行回滚,进行提交后数据库中的自动递增会增加

--- src/main/java/com/java/api/mybatis/main/Main.java
+++ src/main/java/com/java/api/mybatis/main/Main.java
@@ -11,11 +11,32 @@ import com.java.api.mybatis.utils.SqlSessionFactoryUtils;
 public class Main {
 
 	public static void main(String[] args) {
-		testRoleMapper();
-		testRoleMapper2();
-		
+		//testRoleMapper();//xml对应的写法
+		//testRoleMapper2();//接口写法
+		testRoleMapper1();//测试insert、delete、select、update返回的东西  加入东西被回滚
 	}
 	
+	private static void testRoleMapper1() {
+		Logger log = Logger.getLogger(Main.class);
+		SqlSession sqlSession = null;
+		try {
+			sqlSession = SqlSessionFactoryUtils.openSqlSession();
+			RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+			
+			Role role = new Role();
+			role.setRoleName("roleName1");
+			role.setNote("note1");
+			int insertRole = roleMapper.insertRole(role);
+			
+			System.out.println(insertRole);
+			log.info(role.getRoleName());
+			sqlSession.commit();//更改需要对sqlSession进行提交
+		} finally {
+			if (sqlSession != null) {
+				sqlSession.close();
+			}
+		}
+	}
 	
 	private static void testRoleMapper() {
 		Logger log = Logger.getLogger(Main.class);
