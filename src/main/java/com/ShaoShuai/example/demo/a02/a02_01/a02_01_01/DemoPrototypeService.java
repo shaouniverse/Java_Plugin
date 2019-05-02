090b45ce3a208cc4d138698ab1a0b33551d8285e
ShaoShuai <shaopart@163.com>
2019-05-02 12:12:33 +0800

    Bean 的 Scope (能力)    原型和单例

---
+++ src/main/java/com/ShaoShuai/example/demo/a02/a02_01/a02_01_01/DemoPrototypeService.java
@@ -0,0 +1,14 @@
+package com.ShaoShuai.example.demo.a02.a02_01.a02_01_01;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+/**
+ * Prototype的Bean
+ * @author ShaoShuai
+ *
+ */
+@Service
+@Scope("prototype")
+public class DemoPrototypeService {
+
+}
---
+++ src/main/java/com/ShaoShuai/example/demo/a02/a02_01/a02_01_01/DemoSingletonService.java
@@ -0,0 +1,13 @@
+package com.ShaoShuai.example.demo.a02.a02_01.a02_01_01;
+
+import org.springframework.stereotype.Service;
+/**
+ * Singleton 的 Bean
+ * 默认为Singleton,相当于@Scope("singleton")
+ * @author ShaoShuai
+ *
+ */
+@Service
+public class DemoSingletonService {
+	
+}
