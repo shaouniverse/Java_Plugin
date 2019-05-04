c541a3b159062f36f18deb42ff1163c2a6b2d89f
ShaoShuai <shaopart@163.com>
2019-05-04 17:43:05 +0800

    Spring @test

---
+++ src/main/java/com/ShaoShuai/example/demo/a03/a03_07/TestBean.java
@@ -0,0 +1,19 @@
+package com.ShaoShuai.example.demo.a03.a03_07;
+
+public class TestBean {
+	private String content;
+
+	public TestBean(String content) {
+		super();
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
