ca67bdb98e11f72b0dbe81dd979171caaf2a3a3a
ShaoShuai <shaopart@163.com>
2019-05-04 16:57:14 +0800

    条件注解

---
+++ src/main/java/com/ShaoShuai/example/demo/a03/a03_04/ListService.java
@@ -0,0 +1,5 @@
+package com.ShaoShuai.example.demo.a03.a03_04;
+
+public interface ListService {
+	public String showListCmd();
+}
---
+++ src/main/java/com/ShaoShuai/example/demo/a03/a03_04/WindowsListService.java
@@ -0,0 +1,9 @@
+package com.ShaoShuai.example.demo.a03.a03_04;
+
+public class WindowsListService implements ListService {
+
+	public String showListCmd() {
+		return "dir";
+	}
+
+}
