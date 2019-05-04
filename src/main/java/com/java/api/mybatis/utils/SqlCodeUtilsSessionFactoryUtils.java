c59dd1914a85820d5a62b74695905d478dec703c
ShaoShuai <shaopart@163.com>
2019-05-04 17:47:31 +0800

    加密测试

--- src/main/java/com/java/api/mybatis/utils/SqlCodeUtilsSessionFactoryUtils.java
+++ src/main/java/com/java/api/mybatis/utils/SqlCodeUtilsSessionFactoryUtils.java
@@ -94,5 +94,7 @@ public class SqlCodeUtilsSessionFactoryUtils {
 	public static void main(String[] args) {
 		System.out.println(CodeUtils.encode("root"));
 		System.out.println(CodeUtils.encode("123456"));
+		System.out.println(CodeUtils.decode("cm9vdA=="));
+		System.out.println(CodeUtils.decode("MTIzNDU2"));
 	}
 }
